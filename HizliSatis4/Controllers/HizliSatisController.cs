4bca50c954f5e22db9b0785770084416bde25c27
selvasevinc <selvasevinc.ss@gmail.com>
2021-12-01 14:49:35 +0300

    Proje Tamamlandı

---
+++ HizliSatis4/Controllers/HizliSatisController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace HizliSatis4.Controllers
+{
+    public class HizliSatisController : Controller
+    {
+        // GET: HizliSatis
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Index(Models.Email model)
+        {
+            MailMessage mailim = new MailMessage();
+            mailim.To.Add("bilgi@ceptefaturam.com");
+            mailim.From = new MailAddress("bilgi@ceptefaturam.com");
+            mailim.Subject = "Hızlı Satış Programından Mesajınız Var!";
+            mailim.Body = "Ad:" + model.ad + " <br> Mail:" + " " + model.email + " <br>Konu:" + model.konu + " <br>Mesaj:" + model.mesaj;
+            mailim.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential("bilgi@ceptefaturam.com", "0M5azs0@");
+            smtp.Port = 587;
+            smtp.Host = "smtp.ceptefaturam.com";
+            smtp.EnableSsl = false;
+
+            try
+            {
+                smtp.Send(mailim);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return Json("asdasd");
+        }
+
+
+
+        public ActionResult SiparisVeTeklif()
+        {
+            return View();
+        }
+        public ActionResult OtomatikYedekleme()
+        {
+            return View();
+        }
+        public ActionResult ExcelVeriAktarim()
+        {
+            return View();
+        }
+        public ActionResult StokSayim()
+        {
+            return View();
+        }
+        public ActionResult StokRiskLimiti()
+        {
+            return View();
+        }
+        public ActionResult Ozellikler()
+        {
+            return View();
+        }
+        public ActionResult OnMuhasebe()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ HizliSatis4/Models/Email.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HizliSatis4.Models
+{
+    public class Email
+    {
+        public string ad { get; set; }
+        public string email { get; set; }          
+        public string konu { get; set; }
+        public string mesaj { get; set; }
+
+    }
+
+
+    public class Message
+    {
+        public bool IsSuccess { get; set; }
+        public bool message { get; set; }
+    }
+}
